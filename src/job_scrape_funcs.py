f927a5a71969c98ce393d918043adf49adddb29f
Brian Johnson <brjo8465@colorado.edu>
2025-03-31 16:08:24 -0500

    Better exception handling, description matching v1, sample job data

--- src/job_scrape_funcs.py
+++ src/job_scrape_funcs.py
@@ -20,8 +20,8 @@ def get_fresh_cookies(base_url, user_agent):
     cookies["li_gp"] = "MTsxNzQzMTAzMjU1OzA="
     return cookies
 
-def scrape_linkedin_jobs(proxies, pages=3, job_title="Data Scientist"):
-    base_url = f"https://www.linkedin.com/jobs/search?keywords={job_title}&location=Chicago&position=1&pageNum=0"
+def scrape_linkedin_jobs(proxies, pages=3, job_title="Data Scientist", location="Chicago", post_time=1):
+    base_url = f"https://www.linkedin.com/jobs/search?keywords={job_title}&location={location}&f_TPR=r{int(post_time*86400)}&position=1&pageNum=0"
     API_url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?"
     user_agent = UserAgent().random
     jobs = []
@@ -50,53 +50,76 @@ def scrape_linkedin_jobs(proxies, pages=3, job_title="Data Scientist"):
     }
 
     for page in range(pages):
-        url = f"{API_url}keywords={job_title}&location=Chicago&start={int(page*10)}"
-        response = requests.get(url, headers=headers, proxies=proxies, cookies=cookies, timeout=10)
+        try:
+            url = f"{API_url}keywords={job_title}&location={location}&f_TPR=r{int(post_time*86400)}&start={int(page*10)}"
+            response = requests.get(url, headers=headers, proxies=proxies, cookies=cookies, timeout=10)
 
-        if response.status_code != 200:
-            print(f"Failed to retrieve jobs: {response.status_code}")
+            if response.status_code != 200:
+                print(f"Failed to retrieve jobs: {response.status_code}")
+                return jobs
+
+            soup = BeautifulSoup(response.text, "html.parser")
+            job_cards = soup.find_all("div", class_="base-card")
+
+            if not job_cards: #if no jobs found
+                print("No more jobs found.")
+                break
+
+            for job in job_cards:
+                title_elem = job.find("h3", class_="base-search-card__title")
+                company_elem = job.find("h4", class_="base-search-card__subtitle")
+                location_elem = job.find("span", class_="job-search-card__location")
+                time_elem = job.find("time")
+                link_elem = job.find("a", class_="base-card__full-link")  # Job details link
+
+                title = title_elem.text.strip() if title_elem else "N/A"
+                company = company_elem.text.strip() if company_elem else "N/A"
+                location = location_elem.text.strip() if location_elem else "N/A"
+                job_link = link_elem.get("href") if link_elem else "N/A"
+                job_time = time_elem.text.strip() if time_elem else "N/A"
+
+                jobs.append({"Title": title, "Company": company, "Link": job_link, "Posted time": job_time})
+            print(f"{len(jobs)} scraped successfully.")
+            time.sleep(random.uniform(2, 5))
+        except:
+            print("Exception occured.")
             return jobs
-
-        soup = BeautifulSoup(response.text, "html.parser")
-        job_cards = soup.find_all("div", class_="base-card")
-
-        for job in job_cards:
-            title_elem = job.find("h3", class_="base-search-card__title")
-            company_elem = job.find("h4", class_="base-search-card__subtitle")
-            location_elem = job.find("span", class_="job-search-card__location")
-            time_elem = job.find("time")
-            link_elem = job.find("a", class_="base-card__full-link")  # Job details link
-
-            title = title_elem.text.strip() if title_elem else "N/A"
-            company = company_elem.text.strip() if company_elem else "N/A"
-            location = location_elem.text.strip() if location_elem else "N/A"
-            job_link = link_elem.get("href") if link_elem else "N/A"
-            job_time = time_elem.text.strip() if time_elem else "N/A"
-
-            jobs.append({"Title": title, "Company": company, "Link": job_link, "Posted time": job_time})
-        print(f"{len(jobs)} scraped successfully.")
-        time.sleep(random.uniform(1, 5))
     print("Scraping job descriptions...")
+    # Scrape job descriptions if failed previously:
     jobs = scrape_job_descriptions(jobs, proxies)
     print("Finished!")
     return jobs
 
 def scrape_job_descriptions(jobs, proxies):
+    idx = 0
     for job in jobs:
         if "Description" not in job:
+            print(f"Getting job desc number {idx}")
             headers = {"User-Agent": UserAgent().random,
                         "Referer": "https://linkedin.com",
                         }
 
             job_url = job["Link"]
-            response = requests.get(job_url, headers=headers, proxies=proxies)
+            try:
+                response = requests.get(job_url, headers=headers, proxies=proxies)
 
-            if response.status_code != 200:
-                print(f"Failed to retrieve job details: {response.status_code}")
-            else:
-                soup = BeautifulSoup(response.text, "html.parser")
-                job_description_elem = soup.find("div", class_="description__text")
-                job_des = job_description_elem.text.strip().replace("\n","") if job_description_elem else "N/A"
-                job["Description"] = job_des
+                if response.status_code != 200:
+                    print(f"Failed to retrieve job details: {response.status_code}")
+                else:
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    job_description_elem = soup.find("div", class_="description__text")
+                    job_des = job_description_elem.text.strip().replace("\n"," ") if job_description_elem else "N/A"
+                    job["Description"] = job_des
+            except:
+                print("Exception during description scrape.")
+                return jobs
+        idx += 1
 
     return jobs
+
+def check_num_desc(jobs):
+    idx = 0
+    for job in jobs:
+        if "Description" in job:
+            idx += 1
+    return idx
